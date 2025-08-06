79e96ff34789e37d89a8467b2032e689fb94c66a
kang <ksw090711@gmail.com>
2025-08-06 14:18:16 +0900

    Day 4

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface QuestionRepository  extends JpaRepository<Question,Integer> {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
-    Question findByContentLike(String subject, String content);
+    Question findByContentLike(String content);
 }
