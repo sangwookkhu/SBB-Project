0ef4cc2f972956ebef2d41d349b4079a4498a512
kang <ksw090711@gmail.com>
2025-08-07 12:28:21 +0900

    error soulution

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -18,11 +18,8 @@ public class Answer {
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    private LocalDateTime createTime;
+    private LocalDateTime createDate;
 
     @ManyToOne
     private Question question;
-
-    public void setCreateDate(LocalDateTime now) {
-    }
 }
--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -22,7 +22,7 @@ public class Question {
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    private LocalDateTime createTime;
+    private LocalDateTime createDate;
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
